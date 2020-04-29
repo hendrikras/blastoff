cb7a862c77932aa90f2f5f0a5acf03643a489ae8
snkype snkype <hendrik@reinvent.nl>
2020-04-29 19:39:12 +0200

    optimized for different screensizes

--- src/scenes/Enemy.ts
+++ src/scenes/Enemy.ts
@@ -1,35 +1,35 @@
 import { Input, Physics, Math as PMath, Types } from 'phaser';
 export default class Enemy extends Physics.Arcade.Sprite {
     private speed: number = 0;
-    private measure: number;
+    private gridUnit: number;
     private blockY: number = 1;
     private blockX: number = 1;
     private chasePlayer: boolean = true;
-    constructor(config, measure: number) {        
+    constructor(config, gridUnit: number) {        
         super(config.scene, config.x, config.y, "enemy");
          
         config.scene.add.existing(this)
         config.scene.physics.add.existing(this)
         
-        this.speed = measure * 200;
-        this.measure = measure;
-        this.setScale(this.measure / 8);
+        this.speed = gridUnit * 20;
+        this.gridUnit = gridUnit / 10;
+        this.setScale(this.gridUnit / 8);
         this.setCollideWorldBounds(true);
         this.setFrame(1);
         // this.toggleFlipY();
     }        
     public exterminate(player: PMath.Vector2){     
         const enemyVelocity = new PMath.Vector2(player.x - this.x , player.y  - this.y).normalize();
-        // if (this.chasePlayer){
-        //     this.setVelocity(enemyVelocity.x * (this.speed * this.blockX), enemyVelocity.y * (this.speed * this.blockY));   // this.speed = this.measure * 200;
-        // }
+        if (this.chasePlayer){
+            // this.setVelocity(enemyVelocity.x * (this.speed * this.blockX), enemyVelocity.y * (this.speed * this.blockY));   // this.speed = this.gridUnit * 200;
+        }
         
       }
       public isCollidingCrate(){
           return this.body.touching.down || this.body.touching.up || this.body.touching.right || this.body.touching.left  ;
       }
       public cratesOverlap = (me:Enemy, crate: Physics.Arcade.Sprite) => {       
-        console.log('én', me, this);     
+        // console.log('én', me, this);     
         // if (me === this) {
             // me.setVelocity(0);
             // this.speed = 0;
@@ -41,8 +41,11 @@ export default class Enemy extends Physics.Arcade.Sprite {
         this.chasePlayer = !this.chasePlayer;
         if (me === this){            
             // const {touching} = crate.body;
-            if( crate.body.touching.up ){                                 
-                    this.setVelocityY(-this.speed);                                                                  
+            (crate as any).enemyTouching = true;
+            if( crate.body.touching.up ){                                                 
+                    this.setVelocityY(-this.speed);
+                    (crate as any).enemyTouching = false;
+
             }            
             if( crate.body.touching.down ){                                 
                 this.setVelocityY(this.speed);                                                                  
