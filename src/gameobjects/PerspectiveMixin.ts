9f96d74ffedeeb6d5574efb0128461fa477710af
Hendrik <hendrik@reinvent.nl>
2021-03-07 13:19:52 +0100

    added constructor to mixin

--- src/gameobjects/PerspectiveMixin.ts
+++ src/gameobjects/PerspectiveMixin.ts
@@ -1,5 +1,5 @@
 import {Scene, Math as PMath} from 'phaser';
-import {Constructor, lineIntersect} from '../helpers';
+import {Constructor, getGameHeight, getGameWidth, lineIntersect} from '../helpers';
 import Vector2 = Phaser.Math.Vector2;
 import Wall from './Wall';
 
@@ -8,9 +8,49 @@ class extends Base {
 
   private graphics;
   private vanishPoint: Vector2;
-  private point: Vector2;
   private color: number;
-   public update() {
+  private gridUnit: number;
+  private MeasurePointY1: PMath.Vector2;
+  private MeasurePointY2: PMath.Vector2;
+  private MeasurePointX1: PMath.Vector2;
+  private MeasurePointX2: PMath.Vector2;
+  private dimensions: PMath.Vector2;
+  private point: PMath.Vector2;
+  private key;
+
+    constructor(...args: any[]) {
+        super(...args);
+        const scene = args[0].x ? args[0].scene : args[0]; // ?
+        const x = args[1];
+        const y = args[2];
+        this.graphics = scene.add.graphics();
+        this.point = new PMath.Vector2(x, y);
+
+        const {physics: {world: {bounds: {left, right, top, bottom, height, width, centerX, centerY}}}} = scene;
+        const val = (left - right);
+        this.vanishPoint = new PMath.Vector2(centerX, centerY);
+        if (typeof args[3] === 'number') { // toDo: make this distinction based on type
+            const w = args[3];
+            const h = args[4];
+            const d = args[5];
+            this.dimensions = new Vector2(w, h);
+
+            const wrt = (this.dimensions.x / d);
+            const hrt = (this.dimensions.y / d);
+            const aspectH = h < w ? val * hrt : val * hrt;
+            const aspectW = h < w ? val * wrt : val * wrt;
+            this.setMeasurePoints(aspectW, aspectH);
+        } else {
+            this.key = args[3];
+            // this mystery value will take us to the edge of the cube
+            this.gridUnit = scene.data.get('gridUnit');
+            const xy = (this.gridUnit * 2.6) * 4;
+            this.dimensions = new Vector2(xy, xy);
+            this.setMeasurePoints(val, val);
+        }
+    }
+
+   public draw() {
        // @ts-ignore
        const {x, y, dimensions, graphics, vanishPoint, mp } = this;
        graphics.clear();
@@ -42,6 +82,20 @@ class extends Base {
            this.calcDrawDir(top, bottom, floorTop, floorBottom, 'y');
        }
    }
+    private setMeasurePoints(offsetX, offsetY) {
+        // measurepoints are placed on the horizon line of the vanishpoint,
+        // they are spaced equally apart with the vanishpoint in the exact center
+        // when you draw a line from the measurepoint towards the center of the cube it will intersect 2 opposing corners of a face on that cube
+        // when both measurepoints of the same axis are drawn,
+        // an X will then be drawn on that face, starting from the corners and intersecting in the center
+        //
+        // because the game world is not a perfect square. we need to compensate for the aspect ratio.
+
+        this.MeasurePointY1 = new Vector2(this.vanishPoint.x, this.vanishPoint.y + offsetY);
+        this.MeasurePointY2 = new Vector2(this.vanishPoint.x, this.vanishPoint.y - offsetY );
+        this.MeasurePointX1 = new Vector2(this.vanishPoint.x + offsetX, this.vanishPoint.y);
+        this.MeasurePointX2 = new Vector2(this.vanishPoint.x - offsetX, this.vanishPoint.y);
+    }
    private calcDrawDir(top, bottom, floorTop, floorBottom, axis) {
        this.pastCenter(axis)
            ? this.drawPoints(bottom, top, floorBottom, floorTop)
--- src/gameobjects/Wall.ts
+++ src/gameobjects/Wall.ts
@@ -1,18 +1,7 @@
 import {Scene} from 'phaser';
-import {getGameWidth, getGameHeight} from '../helpers';
-import Vector2 = Phaser.Math.Vector2;
 class Wall extends Phaser.GameObjects.Rectangle {
-  private graphics;
-  private vanishPoint: Vector2;
-  private MeasurePointY1: Vector2;
-  private MeasurePointY2: Vector2;
-  private MeasurePointX1: Vector2;
-  private MeasurePointX2: Vector2;
-  private screenHeight: number;
   private key;
   private color: number;
-  private dimensions: Vector2;
-  private point: Vector2;
   constructor(scene: Scene, x: number , y: number, w: number, h: number, d, color: number) {
        super(scene, x, y, w, h, color);
        scene.add.existing(this);
@@ -21,28 +10,9 @@ class Wall extends Phaser.GameObjects.Rectangle {
        this.depth = 1;
        this.key = 'wall';
        this.name = this.key;
-       this.point = new Vector2(0, 0);
-       this.graphics = scene.add.graphics();
-       const height = getGameHeight(scene);
-       const width = getGameWidth(scene);
-       this.screenHeight = height;
-       this.vanishPoint = new Vector2(width / 2, height / 2);
-       this.dimensions = new Vector2(w, h);
 
-       // I always turn to this when I get confused about the math behind one point perspective
-       // and I always get confused when I deal with this
-       // https://www.math.utah.edu/~treiberg/Perspect/Perspect.htm
-       const {physics: {world: {bounds}}} = this.scene;
-       const val = (bounds.left - bounds.right);
-
-       const wrt = (this.dimensions.x / d);
-       const hrt = (this.dimensions.y / d);
-       const aspectH =  h < w ? val * hrt :  val * hrt;
-       const aspectW =  h < w ? val * wrt : val * wrt;
-       this.MeasurePointY1 = new Vector2(this.vanishPoint.x, this.vanishPoint.y + aspectH);
-       this.MeasurePointY2 = new Vector2(this.vanishPoint.x, this.vanishPoint.y - aspectH );
-       this.MeasurePointX1 = new Vector2(this.vanishPoint.x + aspectW, this.vanishPoint.y);
-       this.MeasurePointX2 = new Vector2(this.vanishPoint.x - aspectW, this.vanishPoint.y);
+      // @ts-ignore
+      this.update = this.draw;
    }
 }
 
